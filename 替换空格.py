eb000edb461fae8fab32162555ae46c71fe91f4a
lisidi <sdpure@sina.com>
2018-02-02 15:41:23 +0800

    替换空格

---
+++ 替换空格.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+'''
+请实现一个函数，将一个字符串中的空格替换成“%20”。
+例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+'''
+
+class Solution:
+
+    # 简单代码替换
+    # 在Python中str类型是不可变的类型, 使用replace语句会生成一个新的str, 原始的s还是带空格的str变量
+
+    def replaceSpace(self, s):
+        if type(s) != str:
+            return
+        return  s.replace(' ', '%20')
+
+
+s = 'we are happy'
+test = Solution()
+print(test.replaceSpace(s))
+
